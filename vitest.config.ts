5782ea0ff8cd52b2a84beeb107262849b42d6c05
David Alvarado <dealvarado86@gmail.com>
2025-08-28 12:34:25 -0500

    Add workflow to validate PR tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,21 +3,22 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         globals: true,
-        reporters: ['default'],
+        reporters: ['default', 'github-actions'],
         coverage: {
-            provider: "v8",
-            reporter: ["text", "json", "json-summary", "html"],
-            include: ['src/**'],
+            provider: "v8", // provider
+            reporter: ["text", "json", "json-summary", "html"], // reportes
+            reportOnFailure: true, // report coverage even if fails
+            include: ['src/**'], 
             exclude: [
                 '**/types/**',
                 '**/constants/**',
             ],
-            thresholds: {
+            /*thresholds: {
                 lines: 80,
                 functions: 80,
                 branches: 80,
                 statements: 80,
-            },
+            },*/
         },
     },
 });
